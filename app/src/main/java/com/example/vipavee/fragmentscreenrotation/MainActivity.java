cdf77de5b2494378deda645e17ba9a700ad14225
Vipavee <vi_vpk_4ever@hotmail.com>
2017-08-09 10:16:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/vipavee/fragmentscreenrotation/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.vipavee.fragmentscreenrotation;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import android.app.Fragment;
+
+public class MainActivity extends AppCompatActivity  {
+
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private FragmentManager manager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        manager = getFragmentManager();
+
+    }
+
+
+}
